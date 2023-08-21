eb45a9dbce06bbb87c45f7cddfe80c163a82fab5
Abdelrahman <rafeeq220044@gmail.com>
2023-08-22 00:19:00 +0300

    Add solution for problem `B - Xenia and Ringroad` Link: https://codeforces.com/contest/339/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/339/submission/219844450

--- codeforces/339/B.cpp
+++ codeforces/339/B.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int n, m;
+    int n, m, sum = 0;
     long long r = 0;
     cin >> n >> m;
     vector<int> v(m);
@@ -10,17 +10,21 @@ int main()
     {
         cin >> v[i];
     }
-    int it = 1;
-    int sum = 0;
     for (int i = 0; i < m; i++)
     {
-        sum = v[i] - it;
-        if (sum < 0)
+        if (i == 0)
         {
-            sum += n;
+            r = v[i] - 1;
         }
-        r += sum;
-        it = v[i];
+        else
+        {
+            sum = v[i] - v[i - 1];
+            if (sum < 0)
+            {
+                sum += n;
+            }
+            r += sum;
+        }
     }
     cout << r;
 }
