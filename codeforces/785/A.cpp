05c2b34a7466b68c2910860393296843214f7316
Abdelrahman <rafeeq220044@gmail.com>
2023-08-28 15:54:00 +0300

    Add solution for problem `A - Anton and Polyhedrons` Link: https://codeforces.com/contest/785/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/785/submission/220852339

--- codeforces/785/A.cpp
+++ codeforces/785/A.cpp
@@ -1,27 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-void fast()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-}
+#define azmy ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 int main()
 {
-    fast();
-    int n, sum = 0;
+    azmy
+        map<string, int>
+            mp;
+    mp.insert({"Tetrahedron", 4});
+    mp.insert({"Cube", 6});
+    mp.insert({"Octahedron", 8});
+    mp.insert({"Dodecahedron", 12});
+    mp.insert({"Icosahedron", 20});
+    int n;
     cin >> n;
-    map<string, int> m;
-    m["Tetrahedron"] = 4;
-    m["Cube"] = 6;
-    m["Octahedron"] = 8;
-    m["Dodecahedron"] = 12;
-    m["Icosahedron"] = 20;
-    while (n--)
+    string ch[n];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
     {
-        string s;
-        cin >> s;
-        sum += m[s];
+        cin >> ch[i];
+        sum += mp[ch[i]];
     }
     cout << sum;
 }
